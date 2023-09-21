88b2fbda84baad6f251c025f4e864684339bd117
hora-keven <kevensantos993@gmail.com>
2023-09-21 13:47:18 -0300

    updateRigth

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,25 @@
+import Input from "./Input";
+import Button from "./Button";
+import Header from "./Header";
+function Navbar() {
+  
+    return (
+        <div className="navbar">
+            <Header/>
+            <Input/>
+            <Button/>
+      
+  
+        <div className="windowInputR">
+            <input type="radio"  />
+            <input type="radio" />
+        </div>
+    
+
+            
+        </div>
+
+
+    )
+}
+export default Navbar;
